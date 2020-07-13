4f5a8449ac2d3c92ee7644404ec48789c282f3ae
SeungJun9164 <60804222+SeungJun9164@users.noreply.github.com>
2020-07-13 22:11:52 +0900

    Add files via upload

--- 5_apply_option.py
+++ 5_apply_option.py
@@ -42,72 +42,75 @@ def merge_image():
     # print("간격 : ", cmb_space.get())
     # print("포맷 : ", cmb_format.get())
 
-    # 가로넓이
-    img_width = cmb_width.get()
-    if img_width == "원본유지":
-        img_width = -1 # -1일때는 원본 기준으로 통합
-    else:
-        img_width = int(img_width)
+    try:
+        # 가로넓이
+        img_width = cmb_width.get()
+        if img_width == "원본유지":
+            img_width = -1 # -1일때는 원본 기준으로 통합
+        else:
+            img_width = int(img_width)
 
-    # 간격
-    img_space = cmb_space.get()
-    if img_space == "좁게":
-        img_space = 30
-    elif img_space == "보통":
-        img_space = 60
-    elif img_space == "넓게":
-        img_space = 90
-    else:
-        img_space = 0
+        # 간격
+        img_space = cmb_space.get()
+        if img_space == "좁게":
+            img_space = 30
+        elif img_space == "보통":
+            img_space = 60
+        elif img_space == "넓게":
+            img_space = 90
+        else:
+            img_space = 0
 
-    # 포맷
-    img_format = cmb_format.get().lower() # lower() : 소문자로 변경해주는 메소드
+        # 포맷
+        img_format = cmb_format.get().lower() # lower() : 소문자로 변경해주는 메소드
 
 
-    # print(list_file.get(0, END)) # 모든 파일 목록을 가지고 오기
-    images = [Image.open(x) for x in list_file.get(0, END)]
+        # print(list_file.get(0, END)) # 모든 파일 목록을 가지고 오기
+        images = [Image.open(x) for x in list_file.get(0, END)]
 
-    # 이미지 사이즈 리스트에 넣어서 하나씩 처리
-    image_sizes = [] # (width1, height1), (width2, height2), ....
-    if img_width > -1:
-        image_sizes = [(int(img_width), int(img_width * x.size[1] / x.size[0])) for x in images]
-    else:
-        image_sizes = [(x.size[0], x.size[1]) for x in images] # 원본사이즈 사용
-
-    # size -> size[0] : width, size[1] : height
-    # widths = [x.size[0] for x in images]
-    # heights = [x.size[1] for x in images]
-
-    # 위의 코드와 동일
-    widths, heights = zip(*(image_sizes))
-
-    # max_width : max(widths) widths중에 max값을 찾아 대입 / total_height : sum(heights) heights값을 모두 더한 값을 대입
-    max_width, total_height = max(widths), sum(heights)
-
-    if img_space > 0: # 이미지 간격 옵션 적용
-        total_height += ((img_space * len(images) - 1))
-
-    result_img = Image.new("RGB", (max_width, total_height), (255, 255, 255)) # new : 새로 만들어주는 것 (max_width, total_height)크기만큼 하얀색으로 생성
-    y_offest = 0 # 이미지를 붙일 때 순서
-
-    for idx, img in enumerate(images): # enumerate : images를 열거하여 idx, img 값에 대입
-        # width가 원본이 아닐 경우 이미지 크기 조절
+        # 이미지 사이즈 리스트에 넣어서 하나씩 처리
+        image_sizes = [] # (width1, height1), (width2, height2), ....
         if img_width > -1:
-            img = img.resize(image_sizes[idx]) # resize() : 크기 변경 메소드
+            image_sizes = [(int(img_width), int(img_width * x.size[1] / x.size[0])) for x in images]
+        else:
+            image_sizes = [(x.size[0], x.size[1]) for x in images] # 원본사이즈 사용
 
-        result_img.paste(img, (0, y_offest)) # paste(img, (0, y_offset)) : 붙여넣기(img를 (0, y_offset)위치에)
-        y_offest += (img.size[1] + img_space) # height 값 + 사용가자 지정한 간격 만큼 더해줌
+        # size -> size[0] : width, size[1] : height
+        # widths = [x.size[0] for x in images]
+        # heights = [x.size[1] for x in images]
 
-        progress = (idx + 1) / len(images) * 100 # 실제 퍼센트 정보를 계산
-        p_var.set(progress)
-        progress_bar.update()
+        # 위의 코드와 동일
+        widths, heights = zip(*(image_sizes))
+
+        # max_width : max(widths) widths중에 max값을 찾아 대입 / total_height : sum(heights) heights값을 모두 더한 값을 대입
+        max_width, total_height = max(widths), sum(heights)
+
+        if img_space > 0: # 이미지 간격 옵션 적용
+            total_height += ((img_space * len(images) - 1))
+
+        result_img = Image.new("RGB", (max_width, total_height), (255, 255, 255)) # new : 새로 만들어주는 것 (max_width, total_height)크기만큼 하얀색으로 생성
+        y_offest = 0 # 이미지를 붙일 때 순서
+
+        for idx, img in enumerate(images): # enumerate : images를 열거하여 idx, img 값에 대입
+            # width가 원본이 아닐 경우 이미지 크기 조절
+            if img_width > -1:
+                img = img.resize(image_sizes[idx]) # resize() : 크기 변경 메소드
+
+            result_img.paste(img, (0, y_offest)) # paste(img, (0, y_offset)) : 붙여넣기(img를 (0, y_offset)위치에)
+            y_offest += (img.size[1] + img_space) # height 값 + 사용가자 지정한 간격 만큼 더해줌
+
+            progress = (idx + 1) / len(images) * 100 # 실제 퍼센트 정보를 계산
+            p_var.set(progress)
+            progress_bar.update()
 
 
-    # 포맷 옵션 처리
-    file_name = "SeungJun Images." + img_format
-    dest_path = os.path.join(txt_dest_path.get(), file_name) # dest_path에 txt_dest_path를 얻어와서 join
-    result_img.save(dest_path) # save : dest_path에 저장
-    msgbox.showinfo("알림", "작업이 완료되었습니다.")
+        # 포맷 옵션 처리
+        file_name = "SeungJun Images." + img_format
+        dest_path = os.path.join(txt_dest_path.get(), file_name) # dest_path에 txt_dest_path를 얻어와서 join
+        result_img.save(dest_path) # save : dest_path에 저장
+        msgbox.showinfo("알림", "작업이 완료되었습니다.")
+    except Exception as err: # 예외처리
+        msgbox.showerror("에러", err)
 # 시작
 def start():
     # 각 옵션들 값 확인
@@ -208,6 +211,7 @@ btn_close.pack(side="right", padx=5, pady=5)
 
 btn_start = Button(frame_run, padx=5, pady=5, text="시작", width=12, command=start)
 btn_start.pack(side="right", padx=5, pady=5)
+
 
 root.resizable(False, False)
 root.mainloop()
